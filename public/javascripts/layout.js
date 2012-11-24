9195bfe45e6a9c909b1f4e5ab581ffefcb1d9a6b
Jason Sung <jason@Jason-Sungs-MacBook.local>
2012-11-25 08:26:36 +0900

    made use of html5 audio tag songs now play on the main page

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -6,5 +6,20 @@ $(document).ready(function() {
             document.location.href = '/search/' + $('#search').val();
         }
     });
+    $('.title').click(function (e) {
+        // prevent scrolling 
+        e.preventDefault();
+    });
 });
+
+var changeaudio = function (path) {
+    //var source = "<audio control='controls' id='audioctr'>";
+    //source += "<source src='" + path + "' type='audio/mpeg'>";
+    //source += "</audio>";
+    $('#aud_src').attr('src', path);
+    var aud = $('#audioctr').get(0);
+    aud.pause();
+    aud.load();
+    aud.play();  
+};
 
