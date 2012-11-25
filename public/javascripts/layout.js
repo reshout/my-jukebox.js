e33adb2289282185c222a838843ad0d4e27c03f4
Jason Sung <dythmall@gmail.com>
2012-11-25 22:55:33 +0900

    issue #6 bug fix now it correctly plays the next song TODO: behavior is undefined if you change the songlist... need to fix this

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -9,12 +9,15 @@ $(document).ready(function() {
     });
     
     $('#audioctr').bind('ended', function () {
-        var next = $('#aud_src').attr('data-next');
-        var aud = $('#audioctr').get(0);
-
-        $('#aud_src').attr('src', next);
-        aud.load();
-        aud.play();
+        var id = +$('#aud_src').attr('data-id');
+        var nextSong = $('#song' + id) && $('#song' + id).attr('data-next');
+        var nextId = id + 1;
+        
+        // this is when search result returns nothing
+        if(listLength == 0) nextId = -1;
+        // we've reached the end of the list
+        if(nextId == listLength) nextId = 0;
+        changeaudio(nextSong, nextId); 
     });
 
     preventScrolling();
@@ -27,13 +30,18 @@ var preventScrolling = function () {
     });
 };
 
-var changeaudio = function (path, next) {
-    $('#aud_src').attr('src', path);
-    $('#aud_src').attr('data-next', next);
-    var aud = $('#audioctr').get(0);
-    aud.pause();
-    aud.load();
-    aud.play();  
+var changeaudio = function (path, id) {
+    // in case search result returns nothing we don't play anything
+    if(id == -1) return;
+    var audioSrc = $('#aud_src');
+    var audioCtl = $('#audioctr').get(0);
+    
+    audioSrc.attr('src', path);
+    audioSrc.attr('data-id', id);
+    
+    audioCtl.pause();
+    audioCtl.load();
+    audioCtl.play();  
 };
 
 var changeSongList = function (path) {
